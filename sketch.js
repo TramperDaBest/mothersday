218536ae1d20ba0dd019bf2307df95374e80abbe
TramperDaBest <97362154+TramperDaBest@users.noreply.github.com>
2022-05-08 19:31:36 -0700

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+var5 = 1
+function no(){
+  //do nothing
+}
+
+function love(){
+  for (var count = 0; count < 99999; count++) {
+for (var count1 = 0; count < 99999; count++) {
+for (var count2 = 0; count < 99999; count++) {
+document.write(999999999)
+  document.write(99999)
+}
+}
+  }
+}
